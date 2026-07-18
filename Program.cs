39899b36512fbc842dec71a6ebac91f8c571a449
Jermaine Wiggins <JWiggins973@gmail.com>
2026-07-18 16:44:07 -0400

    added alerts get endpoint to retrieve all db enrecords

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AlertDbContext>();
 var app = builder.Build();
 
+// insert into db
 app.MapPost("/alert", async (AlertEvent evt, AlertDbContext db) =>
 {
     db.Alerts.Add(evt);
     await db.SaveChangesAsync();
     Console.WriteLine($"Saved alert: {evt.Message} | Sound: {evt.SoundValue}");
     return Results.Ok();
+});
+
+// read db
+app.MapGet("/alerts",  async (AlertDbContext db) => 
+{
+    var alerts = await db.Alerts.ToListAsync();
+    return Results.Ok(alerts);
 });
 
 app.Run("http://0.0.0.0:5238");
